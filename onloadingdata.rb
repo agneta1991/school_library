6f71b58de982e9bd9954fd3b2034aab40a1b5654
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-10 15:23:48 +0100

    onload people and rental data

--- onloadingdata.rb
+++ onloadingdata.rb
@@ -15,17 +15,21 @@ class OnloadingData
     end
   end
 
-  def onloading_renatls_data
+  def onloading_rentals_data
     file_name = './rental.json'
     file = File.read(file_name)
     data_hash = JSON.parse(file)
-    puts data_hash
+    data_hash.each do |rental|
+      puts "Date: #{rental['date']} Title: #{rental['title']} by #{rental['author']}"
+    end
   end
 
   def onloading_people_data
     file_name = './people.json'
     file = File.read(file_name)
     data_hash = JSON.parse(file)
-    puts data_hash
+    data_hash.each do |person|
+      puts "[#{person.class.name['type']} Name: #{person['name']} ID: #{person.id['ID']} Age: #{person['age']}
+    end
   end
 end
