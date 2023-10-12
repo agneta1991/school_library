76322f295eb2820a8bc985f80f615d106340b3ae
agneta1991 <pupienytea@gmail.com>
2023-10-12 16:12:41 +0300

    inital start of the tdd test and function

---
+++ tdd/solver_test.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative 'solver'
+
+describe Solver do
+  
+end
+
+# NameError:
+#   uninitialized constant Solver
+# # ./tdd/solver_test.rb:4:in `<top (required)>'
+# No examples found.
