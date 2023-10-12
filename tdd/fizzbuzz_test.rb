89c5557b6f99e543d23e322d2010a81d490e33ff
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-12 16:28:07 +0100

    added test for fizz buzz method which fails

--- tdd/fizzbuzz_test.rb
+++ tdd/fizzbuzz_test.rb
@@ -4,3 +4,9 @@ require_relative 'solver'
 describe Solver do
  describe self.fizzbuzz do
 end
+
+describe '.fizzbuzz' do
+  it 'returns "fizz" for multiples of 3' do
+    expect(Solver.fizzbuzz(9)).to eq('fizz')
+  end
+end
